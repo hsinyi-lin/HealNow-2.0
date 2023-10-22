c26792cdb7ce7b915b4955b84dbee1f84408f201
林欣儀 <59244692+hsinyi-lin@users.noreply.github.com>
2023-10-22 11:52:12 +0800

    refactor(main): drawer

--- lib/main.dart
+++ lib/main.dart
@@ -3,60 +3,72 @@ import 'home.dart';
 import 'map.dart';
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: MyApp(),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('App Title'),
+        title: const Text('App Title'),
       ),
-      body: Myhome(), // 將首頁視圖添加到主頁面
-      drawer: AppDrawer(), // 使用全局抽屜
+      body: const Myhome(), // 將首頁視圖添加到主頁面
+      drawer: const AppDrawer(), // 使用全局抽屜
     );
   }
 }
 
 class AppDrawer extends StatelessWidget {
+  const AppDrawer({Key? key}) : super(key: key);
+
+  void navigateToPage(BuildContext context, Widget page) {
+    Navigator.pop(context); // 關閉抽屜
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => page));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
         children: [
           ListTile(
-            title: Text('首頁'),
+            title: const Text('首頁'),
             onTap: () {
               Navigator.pop(context);
               Navigator.pushReplacement(
                 context,
-                MaterialPageRoute(builder: (context) => MyApp()),
+                MaterialPageRoute(builder: (context) => const MyApp()),
               );
             },
+            // onTap: () {
+            //   navigateToPage(context, MyApp());
+            // },
           ),
           ListTile(
-            title: Text('闢謠專區'),
+            title: const Text('闢謠專區'),
             onTap: () {
               Navigator.pop(context);
             },
           ),
           ListTile(
-            title: Text('身體安全的新聞'),
+            title: const Text('身體安全的新聞'),
             onTap: () {
               Navigator.pop(context);
             },
           ),
           ListTile(
-            title: Text('藥局地圖'),
+            title: const Text('藥局地圖'),
             onTap: () {
               Navigator.pop(context);
               Navigator.pushReplacement(
                 context,
-                MaterialPageRoute(builder: (context) => MapPage()),
+                MaterialPageRoute(builder: (context) => const MapPage()),
               );
             },
           ),
