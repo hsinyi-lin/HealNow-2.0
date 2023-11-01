58b2263ce85cf782272d571b6416f2f2d767a62a
林欣儀 <59244692+hsinyi-lin@users.noreply.github.com>
2023-11-01 18:32:50 +0800

    feat(home): add loading icon

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'med_info.dart'; // 替換為你的詳細資訊頁面的引入
+import 'med_info.dart';
 import 'data/dbhelper.dart';
 
 class Myhome extends StatefulWidget {
@@ -12,43 +11,41 @@ class Myhome extends StatefulWidget {
 }
 
 class _MyhomeState extends State<Myhome> {
-  final TextEditingController _searchController =
-      TextEditingController(); // 創建搜索文字方塊的控制器
-  late DatabaseHelper _databaseHelper; // 創建資料庫説明類的實例
-  List<Map<String, dynamic>> _searchResults = []; // 存儲搜索結果的清單
-  List<Map<String, dynamic>> _allData = []; // 存儲所有資料的清單
+  final TextEditingController _searchController = TextEditingController();
+  late DatabaseHelper _databaseHelper;
+  List<Map<String, dynamic>> _searchResults = [];
+  List<Map<String, dynamic>> _allData = [];
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    _databaseHelper = DatabaseHelper(createDatabaseConnection()); // 初始化資料庫説明類
-    _loadAllData(); // 初始化時載入所有資料
+    _databaseHelper = DatabaseHelper(createDatabaseConnection());
+    _loadAllData();
   }
 
-  // 用於執行資源釋放，dispose 是 State 的生命週期方法之一
   @override
   void dispose() {
-    _searchController.dispose(); // 釋放搜索文字方塊的控制器
+    _searchController.dispose();
     super.dispose();
   }
 
-  // 載入所有資料
   Future<void> _loadAllData() async {
-    await _databaseHelper.openConnection(); // 打開資料庫連接
-    final data = await _databaseHelper.fetchMedData(); // 從資料庫獲取藥物資料
-    await _databaseHelper.closeConnection(); // 關閉資料庫連接
+    await _databaseHelper.openConnection();
+    final data = await _databaseHelper.fetchMedData();
+    await _databaseHelper.closeConnection();
 
     print(data);
 
     setState(() {
-      _allData = data; // 更新所有資料清單
-      _searchResults = data; // 更新搜索結果列表
+      _allData = data;
+      _searchResults = data;
+      isLoading = false;
     });
   }
 
   Future<void> _searchData(String searchTerm) async {
     if (searchTerm.isEmpty) {
-      // 如果搜索文本為空，顯示所有資料
       setState(() {
         _searchResults = _allData;
       });
@@ -60,7 +57,7 @@ class _MyhomeState extends State<Myhome> {
           item['med_en_name'].toLowerCase().contains(searchTerm.toLowerCase()));
 
       setState(() {
-        _searchResults = results.toList(); // 更新搜索結果列表
+        _searchResults = results.toList();
       });
     }
   }
@@ -70,12 +67,11 @@ class _MyhomeState extends State<Myhome> {
       context,
       MaterialPageRoute(
         builder: (context) =>
-            MedPage(title: itemTitle, id: itemId), // 導航到詳細資訊頁面
+            MedPage(title: itemTitle, id: itemId),
       ),
     );
   }
 
-  // 從這個清單中選擇隨機圖示
   final List<IconData> randomIcons = [
     Icons.medical_services,
     Icons.vaccines,
@@ -83,17 +79,16 @@ class _MyhomeState extends State<Myhome> {
     Icons.healing
   ];
 
-  // 獲取隨機圖示的方法
   IconData getRandomIcon() {
     final random = Random();
     final randomIndex = random.nextInt(randomIcons.length);
-    return randomIcons[randomIndex]; // 返回隨機圖示
+    return randomIcons[randomIndex];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 231, 247, 246), // 設置背景顏色
+      backgroundColor: const Color.fromARGB(255, 231, 247, 246),
       body: Column(
         children: <Widget>[
           Padding(
@@ -101,44 +96,49 @@ class _MyhomeState extends State<Myhome> {
             child: TextField(
               controller: _searchController,
               decoration: InputDecoration(
-                hintText: '輸入搜尋文字', // 設置搜索文字方塊的提示文字
+                hintText: '輸入搜尋文字',
                 suffixIcon: IconButton(
-                  icon: const Icon(Icons.search), // 設置搜索圖示
+                  icon: const Icon(Icons.search),
                   onPressed: () {
-                    _searchData(_searchController.text); // 觸發搜索操作
+                    _searchData(_searchController.text);
                   },
                 ),
               ),
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              itemCount: _searchResults.length,
-              itemBuilder: (context, index) {
-                final id = _searchResults[index]['id'];
-                final medTwName = _searchResults[index]['med_tw_name'];
-                final medEnName = _searchResults[index]['med_en_name'];
+            child: isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ListView.builder(
+                    itemCount: _searchResults.length,
+                    itemBuilder: (context, index) {
+                      final id = _searchResults[index]['id'];
+                      final medTwName = _searchResults[index]['med_tw_name'];
+                      final medEnName = _searchResults[index]['med_en_name'];
 
-                return Column(
-                  children: [
-                    ListTile(
-                      leading: Icon(getRandomIcon(),
-                          color: const Color(0xFF1179FA)), // 使用隨機圖示
-                      title: Text('$medTwName'), // 顯示藥物中文名稱
-                      subtitle: Text('$medEnName'), // 顯示藥物英文名稱
-                      onTap: () {
-                        _navigateToDetailPage(medTwName, id); // 點擊時導航到詳細資訊頁面
-                      },
-                    ),
-                    const Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 16.0), // 設置水準內邊距
-                      child: Divider(height: 1, color: Colors.grey), // 添加分隔線
-                    ),
-                  ],
-                );
-              },
-            ),
+                      return Column(
+                        children: [
+                          ListTile(
+                            leading: Icon(getRandomIcon(),
+                                color: const Color(0xFF1179FA)),
+                            title: Text('$medTwName'),
+                            subtitle: Text('$medEnName'),
+                            onTap: () {
+                              _navigateToDetailPage(medTwName, id);
+                            },
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 16.0,
+                            ),
+                            child: Divider(height: 1, color: Colors.grey),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
           ),
         ],
       ),
