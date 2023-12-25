95ef34658b49ec8e85647546dfaa8546d8a80e5d
林欣儀 <59244692+hsinyi-lin@users.noreply.github.com>
2023-12-25 23:59:33 +0800

    refactor home.dart

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'news.dart';
 import 'rumor.dart';
 import 'med.dart';
+import 'pharmacy.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,79 +16,65 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
 
-  @override
-  void initState() {
-    super.initState();
+  // 切換頁面方法
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,  
-      child: Scaffold(
-        body: const Column(
-          children: <Widget>[
-            TabBar(
-              labelColor: Colors.black, 
-              indicatorColor: Colors.black,  
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.medication), 
-                  text: '藥品智庫',
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: <Widget>[
+          const DefaultTabController(
+            length: 3,  
+            child: Column(
+              children: <Widget>[
+                TabBar(
+                  labelColor: Colors.black, 
+                  indicatorColor: Colors.black,  
+                  tabs: [
+                    Tab(icon: Icon(Icons.medication), text: '藥品智庫'),
+                    Tab(icon: Icon(Icons.newspaper), text: '食藥新聞'),
+                    Tab(icon: Icon(Icons.access_alarm), text: '闢謠專區'),
+                  ],
                 ),
-                Tab(
-                  icon: Icon(Icons.newspaper), 
-                  text: '食藥新聞',
-                ),
-                Tab(
-                  icon: Icon(Icons.access_alarm), 
-                  text: '闢謠專區',
+                Expanded(
+                  child: TabBarView(
+                    children: [
+                      MedicationPage(),
+                      NewsPage(),
+                      RumorPage(),
+                    ],
+                  ),
                 ),
               ],
             ),
-            Expanded(
-              child: TabBarView(
-                children: [
-                  MedicationPage(),
-                  NewsPage(),
-                  RumorPage(),
-                ],
-              ),
-            ),
-          ],
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          selectedItemColor: Colors.black,
-          currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: '首頁',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.group),
-              label: '社群',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.local_pharmacy_outlined),
-              label: '藥局',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.bookmark_add_outlined),
-              label: '收藏',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person_4_rounded),
-              label: '個人',
-            ),
-          ],
-        ),
+          ),
+          Container(),
+          // 藥局
+          PharmacyPage(),
+          // 收藏(未來需移動至個人)
+          Container(),
+          // 個人
+          Container(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.black,
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '首頁'),
+          BottomNavigationBarItem(icon: Icon(Icons.group), label: '社群'),
+          BottomNavigationBarItem(icon: Icon(Icons.local_pharmacy_outlined), label: '藥局'),
+          BottomNavigationBarItem(icon: Icon(Icons.local_pharmacy_outlined), label: '收藏'),  // 未來需移動至個人
+          BottomNavigationBarItem(icon: Icon(Icons.person_4_rounded), label: '個人'),
+        ],
       ),
     );
   }
