0779d62ec35d4d4d5e22cadaa3fc6e17a46e7605
林欣儀 <59244692+hsinyi-lin@users.noreply.github.com>
2023-12-27 09:18:16 +0800

    feat: add bookmark

--- lib/screen/med_detail.dart
+++ lib/screen/med_detail.dart
@@ -1,17 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:intl/intl.dart';
 
-String formatDateString(String dateString) {
-  DateFormat inputFormat = DateFormat('EEE, dd MMM yyyy HH:mm:ss \'GMT\'');
-  DateTime dateTime = inputFormat.parse(dateString, true).toLocal();
-  String formattedDate = DateFormat('yyyy-MM-dd').format(dateTime);
-  return formattedDate;
-}
-
-class MedicationDetailPage extends StatelessWidget {
+class MedicationDetailPage extends StatefulWidget {
   final Map<String, dynamic> medication;
 
-  const MedicationDetailPage({Key? key, required this.medication}) : super(key: key);
+  const MedicationDetailPage({Key? key, required this.medication})
+      : super(key: key);
+
+  @override
+  State<MedicationDetailPage> createState() => _MedicationDetailPageState();
+}
+
+class _MedicationDetailPageState extends State<MedicationDetailPage> {
+  bool? isFavorite;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 取得收藏列表進行比對，以顯示收藏icon類型
+    fetchSavedMedications().then((savedMedications) {
+      setState(() {
+        isFavorite = savedMedications
+            .any((savedMed) => savedMed['id'] == widget.medication['id']);
+      });
+    }).catchError((error) {
+      print('Error fetching saved medications: $error');
+    });
+  }
+
+  // 取得收藏列表
+  Future<List<dynamic>> fetchSavedMedications() async {
+    final url = Uri.parse('https://healnow.azurewebsites.net/opendatas/save_class/1');
+    String token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwMjMwMTE5MywianRpIjoiYTIzOTkzZjEtNTNmNy00MWE1LTg5NWQtNmY2ZGU5NWNiNmIyIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjExMTM2MDA0QG50dWIuZWR1LnR3IiwibmJmIjoxNzAyMzAxMTkzLCJleHAiOjE3MDc0ODUxOTN9.4C9f_WxW5uV2JqnOUrK1AidGiQ5hzzr3AnXxPQ5ak00";
+
+    final response = await http.get(
+      url,
+      headers: {
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body)['data'];
+      return data.map((json) => json as Map<String, dynamic>).toList();
+    } else {
+      throw Exception('Failed to load saved medications');
+    }
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final url = Uri.parse(
+        'https://healnow.azurewebsites.net/opendatas/save_class/1/${widget.medication['id']}');
+    String token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwMjMwMTE5MywianRpIjoiYTIzOTkzZjEtNTNmNy00MWE1LTg5NWQtNmY2ZGU5NWNiNmIyIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjExMTM2MDA0QG50dWIuZWR1LnR3IiwibmJmIjoxNzAyMzAxMTkzLCJleHAiOjE3MDc0ODUxOTN9.4C9f_WxW5uV2JqnOUrK1AidGiQ5hzzr3AnXxPQ5ak00";
+
+    http.Response response;
+
+    // 如果 isFavorite 是 true，執行移除收藏
+    if (isFavorite == true) {
+      response = await http.delete(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      );
+    } else {
+      // 如果 isFavorite 是 false，執行新增收藏
+      response = await http.post(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      );
+    }
+
+    if (response.statusCode == 200) {
+      setState(() {
+        // 改變收藏icon
+        isFavorite = !(isFavorite ?? false);
+      });
+    } else {
+      print('Error updating favorite status');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,51 +94,61 @@ class MedicationDetailPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: Text(
-          medication['med_tw_name'],
-          style: TextStyle(color: Colors.black), 
+          widget.medication['med_tw_name'],
+          style: const TextStyle(color: Colors.black),
         ),
-        iconTheme: IconThemeData(color: Colors.black), 
+        iconTheme: const IconThemeData(color: Colors.black),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.bookmark_add), // 收藏图标
-            onPressed: () {
-              // 这里添加点击收藏图标后的行为
-            },
+            icon: Icon(
+                isFavorite == true ? Icons.bookmark : Icons.bookmark_border),
+            onPressed: toggleFavoriteStatus,
           ),
         ],
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            detailItem(Icons.language, '英文名稱', medication['med_en_name']),
-            Divider(),
-            detailItem(Icons.factory, '製造商', medication['manufacturer']),
-            Divider(),
-            detailItem(Icons.flag, '製造商國家', medication['manufacturer_country']),
-            Divider(),
-            detailItem(Icons.category, '藥品類型', medication['med_type']),
-            Divider(),
-            detailItem(Icons.account_circle, '申請人', medication['applicant_name']),
-            Divider(),
-            detailItem(Icons.home_work, '申請人地址', medication['applicant_address']),
-            Divider(),
-            detailItem(Icons.format_list_bulleted, '成分', medication['composition']),
-            Divider(),
-            detailItem(Icons.healing, '使用方法', medication['how_to_use']),
-            Divider(),
-            detailItem(Icons.check_circle_outline, '適應症', medication['indications']),
-            Divider(),
-            detailItem(Icons.confirmation_num, '許可證號', medication['permit_num']),
-            Divider(),
-            detailItem(Icons.card_membership, '許可證類型', medication['permit_type']),
-            Divider(),
-            detailItem(Icons.calendar_today, '許可日期', formatDateString(medication['permit_date'])),
-            Divider(),
-            detailItem(Icons.edit, '編輯日期', formatDateString(medication['edit_date'])),
-            Divider(),
-            detailItem(Icons.date_range, '到期日期', formatDateString(medication['expiration_date'])),
+            detailItem(
+                Icons.language, '英文名稱', widget.medication['med_en_name']),
+            const Divider(),
+            detailItem(Icons.factory, '製造商', widget.medication['manufacturer']),
+            const Divider(),
+            detailItem(
+                Icons.flag, '製造商國家', widget.medication['manufacturer_country']),
+            const Divider(),
+            detailItem(Icons.category, '藥品類型', widget.medication['med_type']),
+            const Divider(),
+            detailItem(Icons.account_circle, '申請人',
+                widget.medication['applicant_name']),
+            const Divider(),
+            detailItem(Icons.home_work, '申請人地址',
+                widget.medication['applicant_address']),
+            const Divider(),
+            detailItem(Icons.format_list_bulleted, '成分',
+                widget.medication['composition']),
+            const Divider(),
+            detailItem(Icons.healing, '使用方法', widget.medication['how_to_use']),
+            const Divider(),
+            detailItem(Icons.check_circle_outline, '適應症',
+                widget.medication['indications']),
+            const Divider(),
+            detailItem(Icons.confirmation_num, '許可證號',
+                widget.medication['permit_num']),
+            const Divider(),
+            detailItem(Icons.card_membership, '許可證類型',
+                widget.medication['permit_type']),
+            const Divider(),
+            detailItem(Icons.calendar_today, '許可日期',
+                formatDateString(widget.medication['permit_date'])),
+            const Divider(),
+            detailItem(Icons.edit, '編輯日期',
+                formatDateString(widget.medication['edit_date'])),
+            const Divider(),
+            detailItem(Icons.date_range, '到期日期',
+                formatDateString(widget.medication['expiration_date'])),
           ],
         ),
       ),
@@ -79,23 +164,30 @@ class MedicationDetailPage extends StatelessWidget {
           Row(
             children: [
               Icon(icon),
-              SizedBox(width: 8),
+              const SizedBox(width: 8),
               Text(
                 title,
-                style: TextStyle(
+                style: const TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 16,
                 ),
               ),
             ],
           ),
-          SizedBox(height: 4), 
+          const SizedBox(height: 4),
           Padding(
-            padding: const EdgeInsets.only(left: 32.0), 
+            padding: const EdgeInsets.only(left: 32.0),
             child: Text(value),
           ),
         ],
       ),
     );
   }
+}
+
+String formatDateString(String dateString) {
+  DateFormat inputFormat = DateFormat('EEE, dd MMM yyyy HH:mm:ss \'GMT\'');
+  DateTime dateTime = inputFormat.parse(dateString, true).toLocal();
+  String formattedDate = DateFormat('yyyy-MM-dd').format(dateTime);
+  return formattedDate;
 }
