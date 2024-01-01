4cc4b018f9780a987f78808f54834baa1faac4a3
林欣儀 <59244692+hsinyi-lin@users.noreply.github.com>
2024-01-01 14:18:23 +0800

    add token(rumor_detail)

--- lib/screen/rumor_detail.dart
+++ lib/screen/rumor_detail.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
 import 'package:url_launcher/url_launcher.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 
 String formatDateString(String dateString) {
   DateFormat inputFormat = DateFormat('EEE, dd MMM yyyy HH:mm:ss \'GMT\'');
@@ -16,36 +15,42 @@ String formatDateString(String dateString) {
 class RumorDetailPage extends StatefulWidget {
   final Map<String, dynamic> rumors;
 
-  const RumorDetailPage({Key? key, required this.rumors})
-      : super(key: key);
+  const RumorDetailPage({Key? key, required this.rumors}) : super(key: key);
 
   @override
   State<RumorDetailPage> createState() => _NewsDetailPageState();
 }
 
 class _NewsDetailPageState extends State<RumorDetailPage> {
+  late String token;
   bool? isFavorite;
 
   @override
   void initState() {
     super.initState();
 
-    // 取得收藏列表進行比對，以顯示收藏icon類型
-    fetchSavedRumors().then((savedRumors) {
-      setState(() {
-        isFavorite = savedRumors
-            .any((savedRumor) => savedRumor['id'] == widget.rumors['id']);
+    loadToken().then((loadedToken) {
+      token = loadedToken; // 在 Future 完成後設置 token
+      // 取得收藏列表進行比對，以顯示收藏icon類型
+      fetchSavedRumors().then((savedRumors) {
+        setState(() {
+          isFavorite = savedRumors
+              .any((savedRumor) => savedRumor['id'] == widget.rumors['id']);
+        });
+      }).catchError((error) {
+        print('Error fetching saved rumors: $error');
       });
-    }).catchError((error) {
-      print('Error fetching saved rumors: $error');
     });
+  }
+
+  Future<String> loadToken() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('token') ?? '';
   }
 
   // 取得收藏列表
   Future<List<dynamic>> fetchSavedRumors() async {
-    final url = Uri.parse('http://127.0.0.1:5000/opendatas/save_class/3');
-    String token =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwMjMwMTE5MywianRpIjoiYTIzOTkzZjEtNTNmNy00MWE1LTg5NWQtNmY2ZGU5NWNiNmIyIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjExMTM2MDA0QG50dWIuZWR1LnR3IiwibmJmIjoxNzAyMzAxMTkzLCJleHAiOjE3MDc0ODUxOTN9.4C9f_WxW5uV2JqnOUrK1AidGiQ5hzzr3AnXxPQ5ak00";
+    final url = Uri.parse('https://healnow.azurewebsites.net/opendatas/save_class/3');
 
     final response = await http.get(
       url,
@@ -64,10 +69,8 @@ class _NewsDetailPageState extends State<RumorDetailPage> {
 
   Future<void> toggleFavoriteStatus() async {
     final url = Uri.parse(
-        'http://127.0.0.1:5000/opendatas/save_class/3/${widget.rumors['id']}');
-    String token =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwMjMwMTE5MywianRpIjoiYTIzOTkzZjEtNTNmNy00MWE1LTg5NWQtNmY2ZGU5NWNiNmIyIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjExMTM2MDA0QG50dWIuZWR1LnR3IiwibmJmIjoxNzAyMzAxMTkzLCJleHAiOjE3MDc0ODUxOTN9.4C9f_WxW5uV2JqnOUrK1AidGiQ5hzzr3AnXxPQ5ak00";
-
+        'https://healnow.azurewebsites.net/opendatas/save_class/3/${widget.rumors['id']}');
+        
     http.Response response;
 
     // 如果 isFavorite 是 true，執行移除收藏
@@ -124,7 +127,8 @@ class _NewsDetailPageState extends State<RumorDetailPage> {
             detailItem(Icons.star_half, widget.rumors['title'],
                 formatDateString(widget.rumors['publish_date'])),
             Divider(),
-            detailItem(Icons.my_library_books, '內文', widget.rumors['content'].replaceAll('。 ', '。\n\n')),
+            detailItem(Icons.my_library_books, '內文',
+                widget.rumors['content'].replaceAll('。 ', '。\n\n')),
             Divider(),
             detailItem(Icons.language, '連結', widget.rumors['url']),
           ],
@@ -135,7 +139,6 @@ class _NewsDetailPageState extends State<RumorDetailPage> {
 
   Widget detailItem(IconData icon, String title, String? value) {
     String displayValue = (value == null || value.isEmpty) ? '未提供' : value;
-    
 
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
