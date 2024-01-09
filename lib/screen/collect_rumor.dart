0576967d020f60371442959a77a32b24b1bcd81b
sharron <10656035@ntub.edu.tw>
2024-01-09 20:28:15 +0800

    Add collect

---
+++ lib/screen/collect_rumor.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import '../services/opendata_service.dart';
+import '../widgets/rumor_card.dart';
+import 'rumor_detail.dart';
+import '../utils/token.dart';
+
+class CollectRumorPage extends StatefulWidget {
+  @override
+  State<CollectRumorPage> createState() => _CollectRumorPageState();
+}
+
+class _CollectRumorPageState extends State<CollectRumorPage> {
+  Future<List<dynamic>>? rumors;
+  List<dynamic> allRumors = [];
+  List<dynamic> filteredRumors = [];
+  String searchQuery = '';
+  late String token;
+
+  @override
+  void initState() {
+    super.initState();
+    loadToken().then((loadedToken) {
+      token = loadedToken;
+
+      setState(() {
+        rumors =
+            OpenDataService().fetchSavedClassList(token, 3).then((rumorList) {
+          allRumors = rumorList;
+          filteredRumors = rumorList;
+          return rumorList;
+        });
+      });
+    });
+  }
+
+  void updateSearchQuery(String newQuery) {
+    setState(() {
+      searchQuery = newQuery;
+      filteredRumors = allRumors.where((rumor) {
+        return rumor['title'].toString().contains(newQuery);
+      }).toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: FutureBuilder<List<dynamic>>(
+                future: rumors,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                    } else if (snapshot.data == null || snapshot.data!.isEmpty) {
+                    return const Center(child: Text('沒有收藏'));
+                  } else {
+                    return ListView.separated(
+                      itemCount: filteredRumors.length,
+                      separatorBuilder: (context, index) => const Divider(),
+                      itemBuilder: (context, index) {
+                        var rumor = filteredRumors[index];
+                        return RumorCard(
+                          rumorDate: rumor['publish_date'],
+                          rumorTitle: rumor['title'],
+                          rumorContent: rumor['content'],
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    RumorDetailPage(rumors: rumor),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
